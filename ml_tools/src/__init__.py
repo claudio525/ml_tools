e4dfa849a19f9f6d9bbb7b6232670d79300bdce9
Claudio <claudio.schill@canterbury.ac.nz>
2020-10-16 14:04:54 +1300

    Added RnadomParamGenerator

---
+++ ml_tools/src/__init__.py
@@ -0,0 +1,1 @@
+from .RandomParamGenerator import RandomParamGenerator
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ml_tools",
+    version="0.1",
+    packages=find_packages(),
+    url="",
+    description="Useful Machine Learning tools",
+)
