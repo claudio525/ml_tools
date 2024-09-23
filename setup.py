cfe4aad8b23dbe1ea693df775688cfe4b1e96959
Claudio <claudio.schill@canterbury.ac.nz>
2024-09-24 07:52:14 +1200

    Major refactor, added lots of documentations. Also added Github action for automatic documentation generation.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="ml_tools",
-    version="0.1",
-    packages=find_packages(),
-    url="",
-    description="Useful Machine Learning tools",
-)
